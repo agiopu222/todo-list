2393433605602926728448cafbe1d00d67c43dae
rebeccaliii <rebeccali823@gmail.com>
2023-07-05 19:44:26 +0800

    modify index.hbs

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose')
 // 載入 handlebars
 const exphbs = require('express-handlebars')
 
+// 載入 Todo model
+const Todo = require('./models/todo') // 載入 Todo model
+
 const app = express()
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
@@ -30,6 +33,14 @@ db.once('open', () => {
 // 設定樣板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: '.hbs' }));
 app.set('view engine', 'hbs')
+
+// Todo路由設定
+app.get('/', (req, res) => {
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
