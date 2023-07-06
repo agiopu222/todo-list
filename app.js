422f05e4e074a706cd8215df60edc3e11d4087e8
rebeccaliii <rebeccali823@gmail.com>
2023-07-06 15:23:16 +0800

    測試create功能,異常

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
 
@@ -60,12 +60,23 @@ app.get('/todos/new', (req, res) => {
 // -----------------------
 
 // CRUD, Create
+// 寫法1
 app.post('/todos', (req, res) => {
   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
+  return Todo.create ({ name: name })     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
+
+// 寫法2
+// app.post('/todos', (req, res) => {
+//   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+//   const todo = new Todo ({name: name})
+//   return todo
+//     .save()
+//     .then(() => res.redirect('/')) // 新增完成後導回首頁
+//     .catch(error => console.log(error))
+// })
 
 // -----------------------
 
@@ -77,6 +88,13 @@ app.get('/todos/:id', (req, res) => {
     .then((todo) => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
+
+// -----------------------
+
+// CRUD, Update
+
+
+
 
 // 設定 port 3000
 app.listen(3000, () => {
