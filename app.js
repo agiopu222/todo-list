58edf57d94a0c848930b087128b9824965c8b006
rebeccaliii <rebeccali823@gmail.com>
2023-07-06 23:00:31 +0800

    modify mongoose config

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 
-// 載入 mongoose
-const mongoose = require('mongoose')
-
 // 載入 handlebars
 const exphbs = require('express-handlebars')
 
 const app = express()
 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+// 引用mongoose檔案
+require('./config/mongoose')
 
 // 設定樣板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: '.hbs' }));
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,26 +1,14 @@
 // app.js 裡和「資料庫連線」有關的程式碼都複製過來一份
 
-// 載入 mongoose
-const mongoose = require('mongoose')
-
 // 載入 todo model
 const Todo = require('../todo')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
 // 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+// 連線資料改為在/config/mongoose, 修改引用位子要注意
+const db = require('../../config/mongoose')
+
 // 連線成功, 新增10筆資料
 db.once('open', () => {
-  console.log('mongodb connected!')
 
   for (let i = 0; i < 10; i++) {
     Todo.create({name:`name-${i}`})
